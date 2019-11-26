0381355a5253484d82b61824d77525f51885f0ad
MATH RUDN <rudn.math@outlook.com>
2019-11-26 11:39:12 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _26._11._19
+{
+    public class StudentGroup
+    {
+        public string Name { get; set; } //свойство Name
+        public List<Student> Students { get; set; } = new List<Student>();
+        
+
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+        public List<int> Ratings { get; set; } = new List<int>();
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+namespace _26._11._19
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var group = new StudentGroup();
+            group.Name = "Group1";
+            for (int i = 1; i < 10; i++)
+            {
+                var student = new Student();
+                student.Name = "Студент " + i;
+                student.LastName = "Last Name" + i;
+                student.Age = 18 + i;
+                group.Students.Add(student);
+            }
+
+            var serializer = new XmlSerializer(typeof(StudentGroup));
+            using (var file = File.Create("students.xml"))
+            {
+                serializer.Serialize(file, group);
+            }
+        }
+    }
+}
